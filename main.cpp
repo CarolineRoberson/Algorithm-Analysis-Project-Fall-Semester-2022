3feae6d4d9ca5e60685de10c76e5f1babed8480a
CarolineRoberson <99359966+CarolineRoberson@users.noreply.github.com>
2022-10-15 23:19:50 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <iostream>
-//#include <list>
+#include <list>
 #include <algorithm>
 #include <array>
 #include <cmath>
+#include <cassert>
 #include "cdpoint.hpp"
 
 using namespace std;
@@ -12,6 +13,8 @@ using namespace cdpoint;
 
 
 cdpoint::cdpoint* fillSet(cdpoint::cdpoint *set, int size){
+	//Generates a full set of random points
+	
 	
 	for(int i = 0; i < size; i++){
 		//Create and add a coordinate pair to each entry in the set
@@ -31,6 +34,7 @@ cdpoint::cdpoint* fillSet(cdpoint::cdpoint *set, int size){
 }
 
 bool inSet(cdpoint::cdpoint coord, cdpoint::cdpoint set[], int size) {
+	//Checks whether the specified coordinate is in the set
 	
 	for(int i = 0; i < size; i++) {
 		
@@ -43,6 +47,7 @@ bool inSet(cdpoint::cdpoint coord, cdpoint::cdpoint set[], int size) {
 }
 
 cdpoint::cdpoint* sort(cdpoint::cdpoint *set, int size) {
+	//Sorts the set in ascending order
 	
 	cdpoint::cdpoint temp;
 	int min;
@@ -65,106 +70,18 @@ cdpoint::cdpoint* sort(cdpoint::cdpoint *set, int size) {
 	
 }
 
-/*
-cdpoint::cdpoint* comparePoints(cdpoint::cdpoint *set, int size) {
-	cdpoint::cdpoint center = cdpoint(0,0);
-	
-	sort(set, size);
-	
-	for(int i = 0; i < size-1; i++){
-		cdpoint::cdpoint p1 = set[i];
-		dpoint::cdpoint p2 = set[i+1];
-		
-		float a1 = getAngle(center, p1);
-		float a2 = getAngle(center, p2);
-		
-		if(a1 > a2) {
-			//p1 comes before p2
-			set[i] = 
-		}
-	}
-}
-*/
-/*
-cdpoint::cdpoint* orderClockwise(cdpoint::cdpoint *set, int size) {
-	//Initialize a centerpoint that will be the average center for the set
-	// of points
-	cdpoint::cdpoint centerpoint = cdpoint(0,0);
-	//Calculate the average center of the set
-	for(int i = 0; i < size; i++) {
-		centerpoint.setx(set[i].getx());
-		centerpoint.sety(set[i].gety());
-	}
-	centerpoint.setx(centerpoint.getx() / size);
-	centerpoint.sety(centerpoint.gety() / size);
-	
-	//Translate all points using the centerpoint to set the center to (0,0)
-	for(int i = 0; i < size; i++) {
-		set[i].setx(set[i].getx() - centerpoint.getx());
-		set[i].sety(set[i].gety() - centerpoint.gety());
-	}
-	
-	//Sort the points in counterclockwise order using the new centerpoint
-	set = comparePoints(set, size);
-	
-	//Translate points back to their original location
-	for(int i = 0; i < size; i++){
-		set[i].setx(set[i].getx() + centerpoint.getx());
-		set[i].sety(set[i].gety() + centerpoint.gety());
-	}
-	
-	return set;
-}
-*/
-
-/*
-cdpoint::cdpoint findCenter(cdpoint::cdpoint *set, int size) {
-	//Initialize a centerpoint that will be the average center for the set
-	// of points
-	cdpoint::cdpoint centerpoint = cdpoint(0,0);
-	//Calculate the average center of the set
-	for(int i = 0; i < size; i++) {
-		centerpoint.setx(set[i].getx());
-		centerpoint.sety(set[i].gety());
-	}
-	centerpoint.setx(centerpoint.getx() / size);
-	centerpoint.sety(centerpoint.gety() / size);
-	
-	return centerpoint;
-}
-
-cdpoint::cdpoint* orderClockwise(cdpoint::cdpoint *set, int size) {
-	cdpoint::cdpoint center = findCenter(set, size);
-	
-	
-	return set;
-}
-*/
-
-double getAngle(cdpoint::cdpoint point) {
-	return atan2(point.getx(), -point.gety());
-}
-
-bool comparePoints(cdpoint::cdpoint p1, cdpoint::cdpoint p2) {
-	return getAngle(p1) < getAngle(p2);
-}
-
 cdpoint::cdpoint* bruteForceCH(cdpoint::cdpoint *set, int size, cdpoint::cdpoint *convexhull){
+	//Calculates the convexhull of a set of points by brute force
+	//Time complexity: O(n^3)
 	
 	int answer_index = 0;
 	
 	//Choose a point to form the base of the line segment
 	for(int i = 0; i < size - 1; i++){
-		cout << "i = " << i << endl;
-		cout << "EXAMINING STARTING POINT " << set[i] <<  " ----------" << endl;
 		
 		//Pick the next point to draw the line segment to
 		//We always pick 1 entry ahead of i because a segment from i to j is the same as j to i
-		for(int j = i+1; j < size; j++){
-			
-			cout << "j = " << j << endl;
-			cout << "	Checking line segment " << set[i] << " to " << set[j] << " ----------" << endl;
-			
+		for(int j = i+1; j < size; j++){			
 			//Calculate the line segment formed by points i and j
 			//using the formula ax + by = c, where a = y2 - y1, b = x1 - x2, and c = x1y2 - y1x2
 			//Point 1 (i) will form (x1, y1) and Point 2 (j) will form (x2, y2)
@@ -183,13 +100,8 @@ cdpoint::cdpoint* bruteForceCH(cdpoint::cdpoint *set, int size, cdpoint::cdpoint
 			bool isHull = true;
 			for(int k = 0; k < size; k++){
 				if((k != i) && (k != j)) { // Skip an iteration if the comparison involves one of the segment endpoints
-					cout << "k = " << k << endl;
-					//cout << "			sign2 = " << sign2 << "; sign1 = " << sign1 << endl;
-					
-					cout << "			Looking at point " << set[k] << endl;
 					
 					float val = (a * set[k].getx()) + (b * set[k].gety()) - c;	//Calculating the formula
-					cout << "				This point value is " << val << endl;
 					
 					//Now check whether the value is above, below, or on the line
 					if(val > 0) isLeft = true;
@@ -200,13 +112,6 @@ cdpoint::cdpoint* bruteForceCH(cdpoint::cdpoint *set, int size, cdpoint::cdpoint
 							break;
 						}
 					}
-					
-					//if((sign2 != -1) && (sign1 != sign2)) {cout << "				Signs don't match. Breaking." << endl; isLine = false; break;}
-					//else {cout << "				Signs match. Moving on." << endl; isLine = true;}
-					
-					//sign2 = sign1;
-				
-				
 				
 				}
 				
@@ -221,18 +126,12 @@ cdpoint::cdpoint* bruteForceCH(cdpoint::cdpoint *set, int size, cdpoint::cdpoint
 			//If we reach here, the line segment must be part of the convex hull, so add it to the set
 			if(isHull) {
 				//cout << "answer_index = " << answer_index << "; answer_index + 1 = " << answer_index+1 << "answer_index + 2 = " << answer_index+2 << endl;
-				cout << "Trying to add points " <<  set[i] << "," << set[j] << endl;
-				if(!inSet(set[j], convexhull, size)) { cout << "Successfully added point " << set[j] << endl; convexhull[answer_index++] = set[j]; }
-				if(!inSet(set[i], convexhull, size)) { cout << "Successfully added point " << set[i] << endl; convexhull[answer_index++] = set[i]; }
-				
-				for(int i = 0; i < size; i ++) { cout << convexhull[i] << " " << endl; }
-	
+				if(!inSet(set[j], convexhull, size)) { convexhull[answer_index++] = set[j]; }
+				if(!inSet(set[i], convexhull, size)) { convexhull[answer_index++] = set[i]; }
 			}
 			
 			
 		}
-		
-				
 		
 		
 	}
@@ -240,32 +139,31 @@ cdpoint::cdpoint* bruteForceCH(cdpoint::cdpoint *set, int size, cdpoint::cdpoint
 	return convexhull;
 }
 
-/*
 float findSide(cdpoint::cdpoint p1, cdpoint::cdpoint p2, cdpoint::cdpoint p3){
 	// x1y2 + x3y1 + x2y3 - x3y2 - x2y1 - x1y3
 	return (p1.getx() * p2.gety()) + (p3.getx() * p1.gety()) - (p3.getx() * p2.gety()) - (p2.getx() * p1.gety()) - (p1.getx() * p3.gety());
 }
 
-void findHull(cdpoint::cdpoint *subset, cdpoint::cdpoint p1, cdpoint::cdpoint p2) {
-	if(subset.empty()) return;
+void findHull(cdpoint::cdpoint *subset, int size, cdpoint::cdpoint p1, cdpoint::cdpoint p2, list<cdpoint::cdpoint>& convexhull) {
+	if(size == 0) { return; }
 	
 	//Find the furthest point from p1<->p2 in subset
 	//Can calculate the distance of the point from the line using the
 	//findSide() function
 	cdpoint::cdpoint furthestPoint;
 	
-	int size = sizeof(subset)/sizeof(subset[0]);	//find length of subset array
 	int maxVal = 0;	//keep track of the largest distance
-	for(int i = 0; i < size - 1; i++){
+	bool pointFound = false;
+	for(int i = 0; i < size; i++){
 		if(findSide(p1, p2, subset[i]) > maxVal) {
+			pointFound = true;
 			maxVal = findSide(p1, p2, subset[i]);
 			furthestPoint = subset[i];
 		}
 	}	
 	
 	//Add furthest point to convexhull between p1/p2
-	
-	
+	if(pointFound) convexhull.push_back(furthestPoint);
 	
 	//Divide remaining points in subset into left and right categories
 	cdpoint::cdpoint s1[size-1];
@@ -279,20 +177,18 @@ void findHull(cdpoint::cdpoint *subset, cdpoint::cdpoint p1, cdpoint::cdpoint p2
 	
 	
 	//Move on to the next subsets
-	findHull(s1, p1, furthestPoint);
-	findHull(s2, furthestPoint, p2);
+	findHull(s1, inds1, p1, furthestPoint, convexhull);
+	findHull(s2, inds2, furthestPoint, p2, convexhull);
 }
-*/
 
-/*
-cdpoint::cdpoint* quickhullCH(cdpoint::cdpoint set, int size, cdpoint::cdpoint *convexhull){
+list<cdpoint::cdpoint>& quickhullCH(cdpoint::cdpoint *set, int size, list<cdpoint::cdpoint>& convexhull){
 	sort(set, size);
 	
-	cdpoint::cdpoint leftpoint = set[0];
-	cdpoint::cdpoint rightpoint = set[size-1];
+	cdpoint::cdpoint leftpoint = set[0]; 
+	cdpoint::cdpoint rightpoint = set[size-1]; 
 	
-	convexhull[0] = leftpoint;
-	convexhull[1] = rightpoint;
+	convexhull.push_back(leftpoint);
+	convexhull.push_back(rightpoint);
 	
 	//divide remaining points into those above and below the line
 	cdpoint::cdpoint s1[size-1];
@@ -300,64 +196,56 @@ cdpoint::cdpoint* quickhullCH(cdpoint::cdpoint set, int size, cdpoint::cdpoint *
 	
 	int inds1 = 0, inds2 = 0;
 	for(int i = 0; i < size - 1; i++){
-		if(findSide(leftpoint, rightpoint, set[i]) > 0) { s1[inds1++] = set[i]; }
-		else if(findSide(leftpoint, rightpoint, set[i]) < 0) { s2[inds2++] = set[i]; }
+		if(findSide(leftpoint, rightpoint, set[i]) > 0) { 
+			s1[inds1++] = set[i]; 
+			}
+		else if(findSide(leftpoint, rightpoint, set[i]) < 0) { 
+			s2[inds2++] = set[i]; 
+			}
 	}
 	
-	findHull(s1, leftpoint, rightpoint);
-	findHull(s2, rightpoint, leftpoint);
+	findHull(s1, inds1, leftpoint, rightpoint, convexhull);
+	findHull(s2, inds2, rightpoint, leftpoint, convexhull);
 	
 	return convexhull;
 	
 	
 }
-*/
 
 int main(int argc, char **argv)
 {
-	
-	//Create an array of 10 - 110 randomized points using the point struct
-	/*int size = rand() % 100 + 10;
-	cdpoint::cdpoint coordSet[size];
-	cout << "Created a set of size " << size << endl;
-	
-	//Create and assign a pointer var to reference the array later on
-	cdpoint::cdpoint *setptr;
-	setptr = coordSet;
-	
-	//Fill the array with randomized points
-	fillSet(coordSet, size);*/
-	
-	//Create a set to store the convex hull
-	int size = 7;
-	cdpoint::cdpoint convexhull[size];
-	//Determine the convex hull with the brute force algorithm
-	//point *bfsetptr = bruteForceSet;		//Passing the function a pointer to indirectly return a list of points with
-	//bruteForceCH(setptr, size, answer);
-	
-	//Determine the convex hull with the quickhull algorithm
-	//list<point> quickhullSet;
-	//point *qhsetptr = quickhullSet;			//Passing the function a pointer to indirectly return a list of points with
-	//quickhullCH(coordSet, size, quickhullSet);
+	 //Input set
+	 int size = 7;
+	 cdpoint::cdpoint set[size] = {{-1,6},{-5,3},{-3,-2},{0,0},{-1,3},{4,-1},{2,5}};
 	 
-	 //cout.precision(2);
-     //cout.setf(ios::fixed);
+	 //Print input set
+	 cout << "Input set: ";
+	 for(int i = 0; i < size; i ++) { cout << set[i] << " "; }
+	 cout << endl << endl;
 	 
-	 cdpoint::cdpoint set[size] = {{-5,3},{0,0},{2,5},{-3,-2},{4,-1},{-1,6},{-1,3}};
-	 //cdpoint::cdpoint set[size] = {{0.0,0.0},{-8.0,3.0},{5.0,-5.0},{1.0,8.0}};
-	 cout<< "Before: " << endl; for(int i = 0; i < size; i ++) { cout << set[i] << " " << endl; }
-	 //sort(set, size);
-	 //cout<< "After: " << endl; for(int i = 0; i < size; i ++) { cout << set[i] << " " << endl; }
-	 //cdpoint::cdpoint convexhull[7];
-	 bruteForceCH(set, size, convexhull);
+	 //Output set for Brute Force
+	 cdpoint::cdpoint BFconvexhull[7];
 	 
-	 cout << "done" << endl;
+	 //Output set for QuickHull
+	 list<cdpoint::cdpoint> QHconvexhull;
 	 
+	 //Compute the convex hull using the brute force algorithm
+	 bruteForceCH(set, size, BFconvexhull);
 	 
-	 for(int i = 0; i < size; i ++) { cout << convexhull[i] << " " << endl; }
+	 //Print the brute force output
+	 cout << "Processing set...\n" << "	Results of brute force: ";
+	 for(int i = 0; i < size; i ++) { if(BFconvexhull[i].getx() > -1000) cout << BFconvexhull[i] << " "; }
+	 cout << endl << endl; 
 	 
-	
-	 //sort(begin(convexhull), end(convexhull), comparePoints);
+	 //Compute the convex hull using the quickhull algorithm
+	 quickhullCH(set, size, QHconvexhull);
+	 
+	 cout << "Processing set...\n" << "	Results of quickhull: ";
+	 list<cdpoint::cdpoint>::iterator it;
+	 for(it = QHconvexhull.begin(); it != QHconvexhull.end(); ++it){
+		 cout << " " << *it;
+	 }
+	 cout << endl << endl; 
 
 	 
 }
